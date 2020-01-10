3bc033ecdfd04bd0245a7bf76c871455889b8786
GintsV <54543100+GintsV@users.noreply.github.com>
2020-01-10 17:04:18 +0200

    v6

--- static/chats.js
+++ static/chats.js
@@ -2,7 +2,7 @@ const ATJAUNOT = 5000;
 async function lasiChatu(){
     const atbilde = await fetch('chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
 
     await new Promise(reslove => setTimeout(reslove, ATJAUNOT));
     await lasiChatu();
@@ -37,7 +37,7 @@ async function suutiZinju(){
     });
     const datuObjekts = await atbilde.json();
 
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
 }
 
 
@@ -48,3 +48,30 @@ ievadesLauks.addEventListener("keyup", function(event){
     }
 
 })
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
